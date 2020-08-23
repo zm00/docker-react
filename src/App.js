f4becbdb5af3330a572dd400b85aed811d0a389a
Sze Mun Foo (AP) <sze-mun.foo@global.ntt>
2020-08-23 12:00:19 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and Make 4th Modification
         </a>
       </header>
     </div>
